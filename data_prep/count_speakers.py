e2f8f84068ee72638b7ae1163fb4eea61bb4dddc
Rohit Gupta <gupta3@gmail.com>
2021-12-02 18:16:48 -0500

    Minor changes

--- data_prep/count_speakers.py
+++ data_prep/count_speakers.py
@@ -1,9 +1,13 @@
 import numpy as np
+from collections import Counter
 
 # This just gets the filenames with speaker referenes (because it is not clear in the filename otherwise)
 dataroot = '../../datasets/flickr_audio/flickr_audio/'
 textin = dataroot + 'wav2spk.txt'
 speaker_files = np.genfromtxt(textin, dtype=[('mystring','S27'),('myint','i8')], delimiter=' ')
 spks = [spk for (filename, spk) in speaker_files]
+
 spks_set = set(spks)
 print(f"Number of unique speakers: {len(spks_set)}")
+
+print(Counter(spks))
